0409524ae4663907ddc1e5ff4d39011bcd691cbb
meltem <m.ataman14@outlook.com>
2025-04-16 23:43:03 -0400

    writing a view and mapping a url to it

--- reviews/views.py
+++ reviews/views.py
@@ -5,3 +5,12 @@ from django.http import HttpResponse
 
 def welcome_view(request):
     return HttpResponse("Welcome to Bookr!")
+
+def query_view(request):
+    value = request.GET.get('q', 'No query')
+    return HttpResponse(f"Query value: {value}")
+
+from django.shortcuts import render
+
+def welcome_view(request):
+    return render(request, "welcome.html")
