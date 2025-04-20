4018095634164d9d84aaafd37952e4fd16f3dbfe
meltem <m.ataman14@outlook.com>
2025-04-20 13:10:23 -0400

    Creating Records with Many-to-Many Relationships

--- reviews/models.py
+++ reviews/models.py
@@ -9,10 +9,19 @@ class Publisher(models.Model):
     def __str__(self):
         return self.name
 
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
 class Book(models.Model):
     title = models.CharField(max_length=70)
     publication_date = models.DateField()
-    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)  # Many-to-One ilişki
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+    authors = models.ManyToManyField(Author)  # 👈 Many-to-Many ilişkisi
 
     def __str__(self):
         return self.title
